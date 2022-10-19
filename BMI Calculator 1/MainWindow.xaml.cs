1c8521a1e74d98b432bf5a5b2ea5c96840380940
hamzaj1127 <hamzaj1127@gmail.com>
2022-10-19 13:46:37 -0400

    dang

--- BMI Calculator 1/MainWindow.xaml.cs
+++ BMI Calculator 1/MainWindow.xaml.cs
@@ -16,16 +16,26 @@ using System.Windows.Shapes;
 
 namespace BMI_Calculator_1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
+        public class Customer
+        {
+            public string lastName { get; set; }
+            public string firstName { get; set; }
+            public string phoneNumber { get; set; }
+            public string heightInches { get; set; }
+            public string weightLbs { get; set; }
+            public string custBMI { get; set; }
+            public string statusTitle { get; set; }
+        }
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        
+       
+        #region Part 1 of the Lab. ClearBtn & ExitBtn
         private void ClearBtn_Click(object sender, RoutedEventArgs e)
         {
             xPhoneNameBox.Text = "";
@@ -39,5 +49,34 @@ namespace BMI_Calculator_1
         {
             Environment.Exit(0);
         }
+        #endregion
+
+        private void SubmitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Customer customer1 = new Customer();
+
+            customer1.lastName = xLastNameBox.Text;
+            customer1.firstName = xFirstNameBox.Text;
+            customer1.phoneNumber = xPhoneNameBox.Text;
+            
+
+            int currentWeight = 0;
+            int currentHeight = 0;
+            Int32.TryParse(xWeightLbsBox.Text, out currentWeight);
+            Int32.TryParse(xHeightInchesBox.Text, out currentHeight);
+            customer1.weightLbs = currentWeight;
+            customer1.heightInches = currentHeight;
+
+            int bmi;
+            bmi = 703 * customer1.weightLbs / (customer1.heightInches * customer1.heightInches);
+            customer1.custBMI = bmi;
+
+            string yourBMIStatus = "NA";
+            customer1.statusTitle = yourBMIStatus;
+
+            MessageBox.Show($"the CUstomer's name is {customer1.firstName} {customer1.lastName} and they can be reached at {customer1.phoneNumber}. They are {customer1.heightInches} inches tall. Their weight is {customer1.weightLbs}. Their BMI is {bmi}");
+        }
+
+        
     }
 }
